71ca9571d20adbd5f2da5661b62e3cf166a3990a
LWH-21 <lwh@free.fr>
2021-11-26 13:13:17 +0100

    Update BubbleSort.js

--- assets/js/sort/BubbleSort.js
+++ assets/js/sort/BubbleSort.js
@@ -7,7 +7,66 @@ function opentab(tname) {
   document.getElementById(tname).style.display = "block";  
 }
 
+function timetosort(nb, v, locale) {
+		nb = nb * ( nb -1) / 4;
+		var ti = nb / v;
+		var oti = ti;
+		var y,d,h,m,s;
+		var res='';
+		y = Math.floor(ti / (3600*24*365));
+		ti = ti % (3600*24*365);
+		d= Math.floor(ti / (3600*24));
+		ti = ti % (3600*24);
+		h = Math.floor(ti / 3600);
+		ti = ti % 3600;
+		m = Math.floor(ti / 60);
+		s=Math.floor(ti % 60);
+		res='';
+		var sy,sm,sd,sh,ss;
+		sy='years';sm='months',sd='days',sh='hours',ss='sec',sm='minutes';
+		if (locale=='fr') {
+			sy='ans';sm='mois',sd='jours',sh='heures',sm='minutes',ss='secondes';
+		} else if (locale='es') {
+			sy='años';sm='meses',sd='días',sh='horas',sm='minutos',ss='segundos';
+		}
+		res="";
+		if (y>0) { res=res+y+" "+sy+" ";}
+		if (d>0) { res=res+d+" "+sd+" ";}
+		if (h>0) { res=res+h+" "+sh+" ";}
+		if (m>0) { res=res+m+" "+sm+" ";}
+		if (s>0) { res=res+s+" "+ss+" ";}
+		if ((s==0) && (m==0) && (h==0) && (d==0) && (y==0)) {
+			res=oti+" "+ss;
+		}
+		return res;
+}
+		
+function calc_sort_speed() {
+		var speed=document.getElementById("computerspeed").value;
+		speed = speed.replace(/ /g, "");
+		if (speed <= 0) { speed = 0 ;}
+		if (isNaN(speed)) { speed = 0;}
+		if ((speed <= 0 ) || (speed> 1E15)) {
+			document.getElementById("computerspeed").value='1 000 000';
+			speed=1000000;
+		}
+
+		console.log('corr',speed);
+		var array = document.getElementById("exectimes").rows; //on récupère les lignes du tableau
+		var linescount = array.length;//on peut donc appliquer la propriété length
+ 
+		for(var i=1; i<linescount; i++)//on peut directement définir la variable i dans la boucle
+		{
+			let Col = array[i].cells;//on récupère les cellules de la ligne
+			let nb = Col[1].innerHTML;
+			nb = nb.replace(/ /g, ""); 
+			Col[2].innerHTML=timetosort(nb, speed,'fr');
+		}
+}
+
 const sortdem = new SortDemo(this,'sort_canvas');
 const algo = new SortAlgo(sortdem);
 sortdem.init(algo);  
 sortdem.renderer.render(sortdem.scene,sortdem.camera);
+
+calc_sort_speed();
